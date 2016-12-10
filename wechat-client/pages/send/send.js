5e1ac299a5e56f436a8b63b0e5381fb1be57c3fe
elly <elly@futunn.com>
2016-12-11 02:38:49 +0800

    提交代码

--- wechat-client/pages/send/send.js
+++ wechat-client/pages/send/send.js
@@ -1,14 +1,29 @@
 Page({
-  data: {
-    height: 20,
-    focus: false
-  },
-  bindButtonTap: function() {
-    this.setData({
-      focus: true
-    })
-  },
-  bindTextAreaBlur: function(e) {
-    console.log(e.detail.value)
-  }
+    data: {
+        height: 20,
+        focus: false,
+        value: ''
+    },
+    bindKeyInput(e) {
+        let content = e.detail.value;
+        if (!content) {
+            this.setData({'value': ''});
+            return;
+        }
+
+        this.setData({'value': content});
+    },
+    sendTopic() {
+        console.log(123, this.data.value);
+        if (!this.data.value) {
+            wx.showModal({
+                title: '提示',
+                content: '请输入吐槽内容～',
+                success: function(res) {}
+            });
+            return;
+        }
+
+        console.log(this.data.value);
+    }
 })
