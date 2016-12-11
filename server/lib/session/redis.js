e16c54e783959cad168657b5f82f778726984f78
sweetvvck <sweetvvck@gmail.com>
2016-12-11 09:18:15 +0800

    add redis session

--- server/lib/session/redis.js
+++ server/lib/session/redis.js
@@ -2,5 +2,9 @@ var RedisStore = require('koa-redis');
 var redisConfig = require('config').get('redis');
 
 module.exports = function () {
-  return new RedisStore(redisConfig);
+  return new RedisStore({
+    "host": redisConfig.host,
+    "port": redisConfig.port,
+    "auth_pass": redisConfig.auth_pass
+  });
 };
